7f599a523dedcdf90ea1860540872fe8213adb61
neelxie <kidricederek@gmail.com>
2021-10-14 19:16:18 +0300

    Add new changes

---
+++ wp-admin/css/colors/blue/frontend/src/config.js
@@ -0,0 +1,5 @@
+// docs Url
+export const DOCS_URL = "https://docs.cranecloud.io";
+export const BLOG_URL = "https://medium.com/cranecloud";
+export const LOGIN_URL = "https://staging.cranecloud.io/login";
+export const REGISTER_URL = "https://staging.cranecloud.io/register";
