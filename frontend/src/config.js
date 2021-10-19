c74f7d1b6cb5050a9a6aa9e0ee7d8388c3208054
neelxie <kidricederek@gmail.com>
2021-10-19 12:28:58 +0300

    remove front end

--- frontend/src/config.js
+++
@@ -1,5 +0,0 @@
-// docs Url
-export const DOCS_URL = "https://docs.cranecloud.io";
-export const BLOG_URL = "https://medium.com/cranecloud";
-export const LOGIN_URL = "https://staging.cranecloud.io/login";
-export const REGISTER_URL = "https://staging.cranecloud.io/register";
